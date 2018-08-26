7c58bd8facbcf6c725d39f382950a94835d28bcd
haotian <ritou11@gmail.com>
2018-08-26 20:59:29 +0800

    pack

--- public/main.js
+++ public/main.js
@@ -1,18 +1,20 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
 
 let mainWindow;
 const createWindow = () => {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-  });
-  mainWindow.loadFile('index.html');
+  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow.loadURL(isDev ? 'http://localhost:3000/' : `file://${path.join(__dirname, '../build/index.html')}`);
   mainWindow.on('closed', () => { mainWindow = null; });
 };
+
 app.on('ready', createWindow);
+
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
 });
+
 app.on('activate', () => {
   if (!mainWindow) createWindow();
 });
